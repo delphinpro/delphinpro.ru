3da40d667f15649760628318b15f6acfaec29c12
delphinpro <delphinpro@yandex.ru>
2019-09-04 17:09:15 +0400

    Update bs-grid-system to 4.0.0

--- gulp.config.js
+++ gulp.config.js
@@ -210,8 +210,8 @@ config.devTools = {
     scriptName: `dev.js`,
 
     overrideScss: [
-        'source/sass/config/_grid.scss',
-        'node_modules/bs-grid-system/source/scss/_bs-grid.scss',
+        'source/sass/config/_bs-grid-config.scss',
+        'node_modules/bs-grid-system/bs-grid.scss',
         'source/sass/config/_vrhythm.scss',
         'node_modules/vrhythm/source/mixins/_rhythm.scss',
     ],
