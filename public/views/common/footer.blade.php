7e750e0a829b855380d90d8d94e8a0aebd1a7e20
delphinpro <delphinpro@yandex.ru>
2019-09-07 02:35:33 +0400

    Add footer of site

--- public/views/common/footer.blade.php
+++ public/views/common/footer.blade.php
@@ -1,2 +1,16 @@
 <footer class="site-footer">
+    <div class="container">
+        <div class="row justify-content-between">
+            <div class="site-footer__column col-md-4 footer-widget">
+                <div class="site-footer__brand footer-widget__header site-branding">
+                    <span class="site-branding__main">
+                        <span class="site-branding__dot">&lt;</span>delphinpro<span class="site-branding__dot">/&gt;</span>
+                    </span>
+                </div>
+                <div class="footer-widget__content footer-widget__info text-muted">
+                    <p>©&nbsp;2011&ndash;{{ date('Y') }}&nbsp;delphinpro</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
