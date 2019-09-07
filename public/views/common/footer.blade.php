292ad044526873a5e4a5e1dac2a6fd8fb0b09838
delphinpro <delphinpro@yandex.ru>
2019-09-07 10:39:43 +0400

    Add social media list

--- public/views/common/footer.blade.php
+++ public/views/common/footer.blade.php
@@ -11,6 +11,30 @@
                     <p>©&nbsp;2011&ndash;{{ date('Y') }}&nbsp;delphinpro</p>
                 </div>
             </div>
+            <div class="site-footer__column col-md-auto footer-widget">
+                <div class="footer-widget__header">
+                    <h3 class="footer-widget__title">Quick links</h3>
+                </div>
+                <div class="footer-widget__content">
+                    <ul class="footer-list">
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Terms of service</a></li>
+                        <li><a href="#">Privacy policy</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="site-footer__column col-md-auto footer-widget">
+                <div class="footer-widget__header">
+                    <h3 class="footer-widget__title">Follow us</h3>
+                </div>
+                <div class="footer-widget__content">
+                    <ul class="social-media-list">
+                        <li><a href="https://mailto"><span class="sr-only">Facebook</span></a></li>
+                        <li><a href="https://t.me"><span class="sr-only">Facebook</span></a></li>
+                        <li><a href="skype:"><span class="sr-only">Facebook</span></a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </div>
 </footer>
