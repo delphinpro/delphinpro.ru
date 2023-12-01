168fb29f13426fdc32855e153832725d1e09c342
delphinpro <delphinpro@yandex.ru>
2023-12-01 09:24:25 +0400

    Доработаны стили

--- resources/views/components/app/logo.blade.php
+++ resources/views/components/app/logo.blade.php
@@ -1,5 +1,5 @@
 <div {{ $attributes->class(['branding']) }}>
     <a class="branding__main" href="/" rel="home">
-        <span class="branding__dot">&lt;</span>delphinpro<span class="branding__dot">/&gt;</span>
+        <span class="branding__dot">&lt;</span>d<span class="branding__part">elphinpro</span><span class="branding__dot">/&gt;</span>
     </a>
 </div>
