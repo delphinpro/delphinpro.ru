732f1caa107f06692c492f01cd41c8c8446a3bcd
delphinpro <delphinpro@yandex.ru>
2024-06-21 22:23:29 +0400

    Добавлена библиотека Axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,17 @@
 /*
  * Site delphinpro.ru
- * Copyright (c) 2019-2023.
+ * Copyright (c) 2019-2024.
  */
 
+import axios from 'axios';
 import './bootstrap';
 
+
+/** @type {HTMLMetaElement} */
+const token = document.head.querySelector('meta[name="csrf-token"]');
+axios.defaults.baseURL = '/';
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
 
 const siteHeader = document.querySelector('.header');
 siteHeader.classList.toggle('is-sticky', scrollY > 0);
