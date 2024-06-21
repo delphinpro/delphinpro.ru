54e369ffe94c48112617e69f1c878545810fe4af
delphinpro <delphinpro@yandex.ru>
2024-06-21 17:17:16 +0400

    Добавлена функция плюрализации для русского языка

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Site delphinpro.ru
- * Copyright (c) 2023.
+ * Copyright (c) 2023-2024.
  */
 
 use Illuminate\Support\Collection;
@@ -24,4 +24,33 @@ function getTimeZoneList(): Collection
 function getUserTimeZone(): ?string
 {
     return auth()?->user()->settings['timezone'] ?? config('app.timezone');
+}
+
+/**
+ * Плюрализация для русского языка
+ *
+ * @param  array  $forms
+ * @param  int    $number
+ *
+ * @return string
+ */
+function pluralize(array $forms, int $number): string
+{
+    [$many, $one, $two] = $forms;
+
+    if ($number >= 5 && $number <= 20) {
+        return $many;
+    }
+
+    $n = $number % 10;
+
+    if ($n === 1) {
+        return $one;
+    }
+
+    if (in_array($n, [2, 3, 4], true)) {
+        return $two;
+    }
+
+    return $many;
 }
