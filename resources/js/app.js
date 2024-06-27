b333e268c40c2b9244e2a816f21f703b5d2d3877
delphinpro <delphinpro@yandex.ru>
2024-06-28 03:09:40 +0400

    Add: Конфигурация перехватчиков Axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,15 +3,45 @@
  * Copyright (c) 2019-2024.
  */
 
+import Toast from '@/toast';
 import axios from 'axios';
 import './bootstrap';
 
+//== PREPARE
 
 /** @type {HTMLMetaElement} */
 const token = document.head.querySelector('meta[name="csrf-token"]');
 axios.defaults.baseURL = '/';
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+
+axios.interceptors.response.use(
+    function (response) {
+        return response.data;
+    },
+    function (err) {
+        let defaultTitle;
+        switch (err.response.status) {
+            case 403:
+                defaultTitle = 'Запрещено';
+                break;
+            case 404:
+                defaultTitle = 'Не найдено';
+                break;
+            default:
+                defaultTitle = 'Ошибка';
+        }
+        Toast.error(
+            err.response.data.message || err.message,
+            err.response.data.title || `${err.response.status} ${defaultTitle}`,
+            { timeout: err.response.status >= 500 ? 0 : 5000 },
+        );
+        return Promise.reject(err);
+    },
+);
+
+
+//== Sticky header
 
 const siteHeader = document.querySelector('.header');
 siteHeader.classList.toggle('is-sticky', scrollY > 0);
