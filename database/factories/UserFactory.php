b489a6634aa07aaabdadcdb4264f3eca3af1e1de
delphinpro <delphinpro@yandex.ru>
2024-09-07 01:13:52 +0400

    fix: Дублирование юзернэймов при сидировании

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,12 +15,9 @@ class UserFactory extends Factory
 {
     public function definition(): array
     {
-        $email = fake()->unique()->email;
-        [$username] = explode('@', $email);
-
         return [
-            'name'              => $username,
-            'email'             => $email,
+            'name'              => $this->faker->unique()->userName,
+            'email'             => $this->faker->unique()->email,
             'email_verified_at' => now(),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'trust_level'       => 0,
